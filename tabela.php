898cd1d7a508092cd3255da61a3665f24c6e9db3
fumireko <fumireko@yandex.com>
2023-10-31 10:49:38 -0300

    Correção dos filtros na tabela

--- tabela.php
+++ tabela.php
@@ -5,19 +5,14 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 @$login = explode(':', $_COOKIE['auth'])[0];
 @$senha = explode(':', $_COOKIE['auth'])[1];
-				
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) die("Conexão falhou: " . $conn->connect_error);
 $sql = "SELECT senha FROM tecnico WHERE login = '$login'";
-	
+
 @$bcrypt = mysqli_fetch_assoc(mysqli_query($conn, $sql))['senha'];
 
-if(isset($_POST['limpar'])){
-	setcookie('auth', '', time()-3600); 
-	ob_start();
-	header("Refresh: 0"); 
-	ob_end_flush();
-}
+if(isset($_POST['limpar'])){ setcookie('auth', '', time()-3600); header("Refresh: 0"); }
 ?>
 
 <!DOCTYPE html>
@@ -52,93 +47,81 @@ if(isset($_POST['limpar'])){
 </head>
 <body>
 	<?php if(isset($_COOKIE['auth']) && ($senha = $bcrypt || password_verify($senha, $bcrypt))): ?>
-	
-	<script>
-	async function progressBar(interval) {
-	  $('.progress-bar').css('width', '0%');
-	  const increment = 100 / interval;
-	  let progress = 0;
+	<div id="table-container"></div>
 
-	  for (let i = 0; i <= interval; i++) {
-		$('.progress-bar').css('width', progress + '%');
-		console.log(progress);
-		await new Promise(resolve => setTimeout(resolve, 25));
-		progress += increment;
-	  }
+	<div class="bg-dark text-light">
+	<div id="filtros" class="row justify-content-center p-3 px-5 mx-5">
+		<div class="col-3">
+		  <label for="codigo-filter">Código:</label>
+		  <select class="form-control" id="codigo-filter">
+			<option value="">Todos</option>
+		  </select>
+		</div>
+		
+		<div class="col-3">
+		  <label for="descricao-filter">Descrição:</label>
+		  <select class="form-control" id="descricao-filter">
+			<option value="">Todas</option>
+		  </select>
+		</div>
+		
+		<div class="col-3">
+		  <label for="endereco-filter">Endereço:</label>
+		  <select class="form-control" type="text" id="endereco-filter">
+			<option value="">Todos</option>
+		  </select>
+		</div>
 
-	  // Garante que a barra de progresso atinja 100% no final
-	  $('.progress-bar').css('width', '100%');
-	  console.log('100');
-	}
-	progressBar(20);
-	</script>
-	
-	<div class="loading-bar" style="height: 4px">
-	  <div class="progress" style="height: 4px">
-		<div class="progress-bar" role="progressbar" style="height: 4px"></div>
-	  </div>
+		<div class="col-3">
+		  <label for="tecnico-filter">Técnico:</label>
+		  <select class="form-control" id="tecnico-filter">
+			<option value="">Todos</option>
+		  </select>
+		</div>
+		
+		<div class="col-3">
+		  <label for="setor-filter">Setor:</label>
+		  <select class="form-control" id="setor-filter">
+			<option value="">Todos</option>
+		  </select>
+		</div>
+		
+		<div class="col-3">
+		  <label for="nis-filter">NIS:</label>
+		  <select class="form-control" id="nis-filter">
+			<option value="">Todos</option>
+		  </select>
+		</div>
+		
+		<div class="col-3">
+		  <label for="nome-filter">Nome:</label>
+		  <select class="form-control" id="nome-filter">
+			<option value="">Todos</option>
+		  </select>
+		</div>
+
+		<div class="col-3">
+		  <label for="data-atendimento-filter">Data de Atendimento:</label>
+		  <select class="form-control" id="data-atendimento-filter">
+			<option value="">Todas</option>
+		  </select>
+		</div>
+		
+	</div>
 	</div>
 	
 	<div id="table-container"></div>
-
-	<nav class="navbar fixed-bottom navbar-dark bg-dark pb-4 text-light">
-		<div class="container-fluid row d-flex justify-content-center px-2 mx-2">
-			<div class="col-2">
-			  <label for="nis-filter">NIS:</label>
-			  <select class="form-control" id="nis-filter">
-				<option value="">Todos</option>
-			  </select>
-			</div>
-			<div class="col-2">
-			  <label for="data-atendimento-filter">Data de Atendimento:</label>
-			  <select class="form-control" id="data-atendimento-filter">
-				<option value="">Todas</option>
-			  </select>
-			</div>
-			<div class="col-2">
-			  <label for="endereco-filter">Endereço:</label>
-			  <input class="form-control" type="text" id="endereco-filter" name="endereco-filter" list="list-enderecos">
-			  <datalist id="list-enderecos"></datalist>
-			</div>
-			<div class="col-2">
-			  <label for="setor-filter">Setor:</label>
-			  <select class="form-control" id="setor-filter">
-				<option value="">Todos</option>
-			  </select>
-			</div>
-			<div class="col-2">
-			  <label for="tecnico-filter">Técnico:</label>
-			  <select class="form-control" id="tecnico-filter">
-				<option value="">Todos</option>
-			  </select>
-			</div>
-			<div class="col-2">
-			  <label for="descricao-filter">Descrição:</label>
-			  <select class="form-control" id="descricao-filter">
-				<option value="">Todas</option>
-			  </select>
-			</div>
-			<button class="btn btn-sm btn-primary col-3 mt-2" id="filtro-endereco">Filtrar</button>
-		</div>
-	</nav>
 	<?php else: header("Location: login.php"); ?>
 	<?php endif; ?>
 </body>
     <script>
-	function addDatalistOption(datalistId, value) {
-	  var datalist = $(datalistId);
-	  if (datalist.find('option[value="' + value + '"]').length === 0) {
-		$('<option>').attr('value', value).appendTo(datalist);
-	  }
-	}
-
 	function addSelectOption(selectId, value) {
 	  var select = $(selectId);
 	  if (select.find('option[value="' + value + '"]').length === 0) {
 		$('<option>').attr('value', value).text(value).appendTo(select);
 	  }
 	}
-	
+
 	function compareRowsAsc(row1, row2, index) {
 	  var tdValue1 = $(row1).find('td').eq(index).text();
 	  var tdValue2 = $(row2).find('td').eq(index).text();
@@ -162,94 +145,97 @@ if(isset($_POST['limpar'])){
 		tbody.append(row);
 	  });
 	}
-	
+
 	$(document).ready(function() {
-	  $('.loading-bar').show();			  
 	  $.ajax({
 		url: '/api/atendimentos',
 		type: 'GET',
 		dataType: 'json',
 		success: function(data) {
 		  // Cria a tabela
-		  $('.progress-bar').css('width', 100 + '%');
 		  var table = $('<table>');
 		  table.addClass('table');
 		  table.addClass('table-hover');
 		  var thead = $('<thead>').appendTo(table);
 		  var tbody = $('<tbody>').appendTo(table);
-		  
+
 		  // Cria as colunas da tabela
-		  var columns = ['Nome', 'NIS', 'Data', 'Endereço', 'Setor', 'Técnico', 'Descrição'];
+		  var columns = ['Código', 'Descrição', 'Endereço', 'Técnico', 'Setor', 'NIS', 'Nome', 'Data'];
 		  var tr = $('<tr>').appendTo(thead);
 		  for (var i = 0; i < columns.length; i++) {
 			$('<th>').text(columns[i]).appendTo(tr);
 		  }
-		  
+
 		  // Preenche a tabela com os dados
 		  $.each(data, function(index, item) {
 			var tr = $('<tr>').appendTo(tbody);
-			$('<td>').text(item.nome).appendTo(tr);
-			$('<td>').text(item.nis).appendTo(tr);
-			$('<td>').text(item.data_atendimento).appendTo(tr);
-			$('<td>').html('<a href="api/enderecos?logradouro=' + item.endereco + '">' + item.endereco + '</a>').appendTo(tr);
-			$('<td>').text(item.setor).appendTo(tr);
-			$('<td>').text(item.tecnico).appendTo(tr);
+			$('<td>').text(item.codigo).appendTo(tr);
 			$('<td>').text(item.descricao).appendTo(tr);
+			$('<td>').html('<a href="api/enderecos?logradouro=' + item.endereco + '">' + item.endereco + '</a>').appendTo(tr);
+			$('<td>').text(item.tecnico).appendTo(tr);
+			$('<td>').text(item.setor).appendTo(tr);
+			$('<td>').text(item.nis).appendTo(tr);
+			$('<td>').text(item.nome).appendTo(tr);
+			$('<td>').text(item.data_atendimento).appendTo(tr);
 		  });
-		  
+
 		  // Adiciona a tabela ao elemento HTML
 		  $('#table-container').append(table);
-		  
+
 		  // Filtra os dados na tabela
 		  $('select').on('change', function() {
-			var nis = $('#nis-filter').val();
-			var dataAtendimento = $('#data-atendimento-filter').val();
-			var endereco = $('#endereco-filter').val();
-			var setor = $('#setor-filter').val();
-			var tecnico = $('#tecnico-filter').val();
+			var codigo = $('#codigo-filter').val();
 			var descricao = $('#descricao-filter').val();
-			
+			var endereco = $('#endereco-filter').val();
+			var tecnico = $('#tecnico-filter').val();
+			var setor = $('#setor-filter').val();
+			var nis = $('#nis-filter').val();
+			var nome = $('#nome-filter').val();
+			var dataAtendimento = $('#data-atendimento-filter').val();
+
 			$('tbody tr').each(function(index, item) {
 			  var showRow = true;
-			  
-			  if (nis && $(item).find('td:nth-child(1)').text() !== nis) {
+
+			  if (codigo && $(item).find('td:nth-child(1)').text() !== codigo)
 				showRow = false;
-			  }
 			  
-			  if (dataAtendimento && $(item).find('td:nth-child(2)').text() !== dataAtendimento) {
+			  if (descricao && $(item).find('td:nth-child(2)').text() !== descricao)
 				showRow = false;
-			  }
-			  
-			  if (endereco && $(item).find('td:nth-child(3)').text() !== endereco) {
+
+			  if (endereco && $(item).find('td:nth-child(3)').text() !== endereco)
 				showRow = false;
-			  }
-			  
-			  if (setor && $(item).find('td:nth-child(4)').text() !== setor) {
+			
+			  if (tecnico && $(item).find('td:nth-child(4)').text() !== tecnico)
 				showRow = false;
-			  }
-			  
-			  if (tecnico && $(item).find('td:nth-child(5)').text() !== tecnico) {
+			
+			  if (setor && $(item).find('td:nth-child(5)').text() !== setor)
 				showRow = false;
-			  }
-			  
-			  if (descricao && $(item).find('td:nth-child(6)').text() !== descricao) {
+			
+			  if (nis && $(item).find('td:nth-child(6)').text() !== nis)
 				showRow = false;
-			  }
+			
+			  if (nome && $(item).find('td:nth-child(7)').text() !== nome)
+				showRow = false;
 			  
+			  if (dataAtendimento && $(item).find('td:nth-child(8)').text() !== dataAtendimento)
+				showRow = false;
+
 			  $(item).toggle(showRow);
 			});
 		  });
-		  
+
 		  // Preenche os selects com as opções de filtro
 		  $.each(data, function(index, item) {
-			addSelectOption('#nis-filter', item.nis);
-			addSelectOption('#data-atendimento-filter', item.data_atendimento);
-			addDatalistOption('#list-enderecos', item.endereco);
-			addSelectOption('#setor-filter', item.setor);
-			addSelectOption('#tecnico-filter', item.tecnico);
+			addSelectOption('#codigo-filter', item.codigo);
 			addSelectOption('#descricao-filter', item.descricao);
+			addSelectOption('#endereco-filter', item.endereco);
+			addSelectOption('#tecnico-filter', item.tecnico);
+			addSelectOption('#setor-filter', item.setor);
+			addSelectOption('#nis-filter', item.nis);
+			addSelectOption('#nome-filter', item.nome);
+			addSelectOption('#data-atendimento-filter', item.data_atendimento);
 		  });	    
-			
+
 		  var ths = $('.table th');
 		  ths.each(function(index, th) {
 			  $(th).on('click', function() {
@@ -263,53 +249,12 @@ if(isset($_POST['limpar'])){
 				  // Adiciona a seta na coluna atual
 			  $('<span>').addClass('arrow').appendTo(th);
           });
-		  //$('.loading-bar').hide();
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
 		  console.error(textStatus + ': ' + errorThrown);
-		  // Oculta a barra de carregamento em caso de erro
-		  $('.loading-bar').hide();
 		}
-	  });
-	});
-	
-	$('#filtro-endereco').on('click', function() {
-	  var nis = $('#nis-filter').val();
-	  var dataAtendimento = $('#data-atendimento-filter').val();
-	  var endereco = $('#endereco-filter').val();
-	  var setor = $('#setor-filter').val();
-	  var tecnico = $('#tecnico-filter').val();
-	  var descricao = $('#descricao-filter').val();
-
-	  $('tbody tr').each(function(index, item) {
-		var showRow = true;
-
-		if (nis && $(item).find('td:nth-child(1)').text() !== nis) {
-		  showRow = false;
-		}
-
-		if (dataAtendimento && $(item).find('td:nth-child(2)').text() !== dataAtendimento) {
-		  showRow = false;
-		}
-
-		if (endereco && $(item).find('td:nth-child(3)').text() !== endereco) {
-		  showRow = false;
-		}
-
-		if (setor && $(item).find('td:nth-child(4)').text() !== setor) {
-		  showRow = false;
-		}
-
-		if (tecnico && $(item).find('td:nth-child(5)').text() !== tecnico) {
-		  showRow = false;
-		}
-
-		if (descricao && $(item).find('td:nth-child(6)').text() !== descricao) {
-		  showRow = false;
-		}
-
-		$(item).toggle(showRow);
 	  });
 	});
 	</script>
-</html>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html> 
